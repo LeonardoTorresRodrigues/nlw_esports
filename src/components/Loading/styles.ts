ac033c5356cd101c932ddcb15ef841527b3e89e8
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2022-09-16 11:58:22 -0300

    Tela de loading

--- src/components/Loading/styles.ts
+++ src/components/Loading/styles.ts
@@ -1,9 +1,9 @@
-import { THEME } from './../../theme';
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: THEME.COLORS.BACKGROUND_800
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 });
